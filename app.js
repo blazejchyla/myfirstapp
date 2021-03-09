c7d88b426b4064be19b8a180fa01c87a571f6139
Skervesen <80217475+Skervesen@users.noreply.github.com>
2021-03-09 11:22:59 +0100

    update

--- app.js
+++ app.js
@@ -31,10 +31,22 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 // Handlebards helpers
-const { formatDate } = require('./helpers/hbs')
+const { formatDate, stripTags, truncate, editIcon } = require('./helpers/hbs')
 
 // Handlebars
-app.engine('.hbs', exphbs({helpers: { formatDate },defaultLayout: 'main', extname: '.hbs'}));
+app.engine(
+  '.hbs',
+  exphbs({helpers: { 
+    formatDate,
+    stripTags,
+    truncate,
+    editIcon,
+  },
+  defaultLayout: 'main', 
+  extname: '.hbs',
+})
+)
+
 app.set('view engine', '.hbs');
 
 // Sessions
@@ -49,6 +61,12 @@ app.use(session({
 // Passport middleware
 app.use(passport.initialize())
 app.use(passport.session())
+
+// Set global var
+app.use(function (req, res, next) {
+  res.locals.user = req.user || null
+  next()
+})
 
 // Static folder
 app.use(express.static(path.join(__dirname, 'public')))
